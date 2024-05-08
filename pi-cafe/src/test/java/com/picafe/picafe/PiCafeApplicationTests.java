84019b4acc72edbe43d7d633644fb2b8c008443b
SmarthSachdeva <101968647+SmarthSachdeva@users.noreply.github.com>
2024-05-08 12:22:48 +0530

    Add files via upload

---
+++ pi-cafe/src/test/java/com/picafe/picafe/PiCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picafe.picafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
